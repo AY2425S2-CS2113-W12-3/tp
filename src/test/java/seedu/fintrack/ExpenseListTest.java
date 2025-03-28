d09297458f701b4c325e82c5daf7a97f9dd33197
Kai Xiang <yew.kaixiang@u.nus.edu>
2025-03-29 00:41:30 +0800

    Manage JUnit Tests
    -Added Junit tests for CommandTests primarily in an earlier commit
    -Arrange all the Junit tests for respective classes in different files

--- src/test/java/seedu/fintrack/ExpenseListTest.java
+++ src/test/java/seedu/fintrack/ExpenseListTest.java
@@ -1,4 +1,27 @@
 package seedu.fintrack;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ExpenseListTest {
+    private ExpenseList expenseList = new ExpenseList();
+
+    @Test
+    void addExpense_storesExpenseCorrectly() {
+        Expense expense = new Expense(2000, "Food", "Dinner", new Date());
+        expenseList.addExpense(expense);
+        assertEquals(expense, expenseList.getExpense(0));
+    }
+
+    @Test
+    void deleteExpense_removesExpenseFromList() {
+        Expense expense = new Expense(1500, "Transport", "Taxi", new Date());
+        expenseList.addExpense(expense);
+        expenseList.deleteExpense(expense);
+        assertThrows(IndexOutOfBoundsException.class, () -> expenseList.getExpense(0));
+    }
 }
--- src/test/java/seedu/fintrack/UiTest.java
+++ src/test/java/seedu/fintrack/UiTest.java
@@ -1,4 +1,15 @@
 package seedu.fintrack;
 
+
+
 public class UiTest {
 }
+
+//    @Test
+//    void printGreeting_displaysGreetingMessage() {
+//        Ui.printGreeting();
+//
+//        assertTrue(outputStream.toString().contains("Hello, I am Fin!"));
+//    }
+//}
+
