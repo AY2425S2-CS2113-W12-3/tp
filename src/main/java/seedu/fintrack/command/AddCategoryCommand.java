b6d167e67aef29ce1c9a6ee13cf0102585ebdc6d
sjwking <kingsleyser@gmail.com>
2025-04-05 23:52:45 +0800

    Add more error handling for categories

--- src/main/java/seedu/fintrack/command/AddCategoryCommand.java
+++ src/main/java/seedu/fintrack/command/AddCategoryCommand.java
@@ -17,6 +17,27 @@ public class AddCategoryCommand implements Command {
     @Override
     public void execute(ExpenseList expenseList, Ui ui, Storage storage, Categories categories)
             throws FinTrackException {
+
+        // Check if expenseList is null
+        if (expenseList == null) {
+            throw new FinTrackException("Expense list is null");
+        }
+
+        // Check if parser is null
+        if (parser == null) {
+            throw new FinTrackException("Parser is null");
+        }
+
+        // Check if storage is null
+        if (storage == null) {
+            throw new FinTrackException("Storage is null");
+        }
+
+        // Check if categories is null
+        if (categories == null) {
+            throw new FinTrackException("Categories is null");
+        }
+
         String newCategory = parser.promptInput("Please enter the name of the new category:");
         if (newCategory.isEmpty()) {
             ui.showError("Empty input. Please enter a category.");
--- src/main/java/seedu/fintrack/command/DeleteCategoryCommand.java
+++ src/main/java/seedu/fintrack/command/DeleteCategoryCommand.java
@@ -17,6 +17,27 @@ public class DeleteCategoryCommand implements Command {
     @Override
     public void execute(ExpenseList expenseList, Ui ui, Storage storage, Categories categories)
             throws FinTrackException {
+
+        // Check if expenseList is null
+        if (expenseList == null) {
+            throw new FinTrackException("Expense list is null");
+        }
+
+        // Check if parser is null
+        if (parser == null) {
+            throw new FinTrackException("Parser is null");
+        }
+
+        // Check if storage is null
+        if (storage == null) {
+            throw new FinTrackException("Storage is null");
+        }
+
+        // Check if categories is null
+        if (categories == null) {
+            throw new FinTrackException("Categories is null");
+        }
+
         categories.printCustomCategories();
         int numberOfDefaultCategories = 5;
         if (categories.size() == numberOfDefaultCategories) {
