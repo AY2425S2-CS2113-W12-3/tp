b92f84c1307f2ff9d9f2646ee7f44996cd8fca30
sjwking <kingsleyser@gmail.com>
2025-04-05 23:29:32 +0800

    Add support for default and custom categories

--- src/main/java/seedu/fintrack/command/DeleteCategoryCommand.java
+++ src/main/java/seedu/fintrack/command/DeleteCategoryCommand.java
@@ -17,19 +17,20 @@ public class DeleteCategoryCommand implements Command {
     @Override
     public void execute(ExpenseList expenseList, Ui ui, Storage storage, Categories categories)
             throws FinTrackException {
-        categories.printCategories();
-        if (categories.getCategories().isEmpty()) {
+        categories.printCustomCategories();
+        int numberOfDefaultCategories = 5;
+        if (categories.size() == numberOfDefaultCategories) {
             ui.printBorder();
             return;
         }
         int index = parser.readInt("Enter the index of the category to delete:");
-        if (index <= 0 || index > categories.size()) {
+        if (index <= 0 || index > categories.size() - numberOfDefaultCategories) {
             ui.showError("Invalid index.");
             ui.printBorder();
             return;
         }
-        String deletedCategory = categories.getCategory(index);
-        categories.removeCategory(index);
+        String deletedCategory = categories.getCategory(index + numberOfDefaultCategories);
+        categories.removeCategory(index + numberOfDefaultCategories);
         ui.showMessage(deletedCategory + " has been deleted.");
         storage.saveCategoriesToFile();
         ui.printBorder();
