552fb0be3978dbcaa7a7f631a2fb5e992a2b9a92
Akaash Dhayalan <akaashdhayalan243@gmail.com>
2025-04-08 06:45:46 +0800

    Fix bugs with reagrds to income.

--- src/main/java/seedu/fintrack/command/UpdateIncomeCommand.java
+++ src/main/java/seedu/fintrack/command/UpdateIncomeCommand.java
@@ -1,3 +1,4 @@
+//@@author akaash02
 package seedu.fintrack.command;
 
 import seedu.fintrack.ExpenseList;
@@ -9,7 +10,9 @@ import seedu.fintrack.Categories;
 import seedu.fintrack.Savings;
 
 public class UpdateIncomeCommand implements Command {
+    private static final int MAX_EXPENSE_AMOUNT = 100000000; // 1 million dollars in cents
     private final Parser parser;
+
 
     public UpdateIncomeCommand(Parser parser) {
         this.parser = parser;
@@ -18,17 +21,33 @@ public class UpdateIncomeCommand implements Command {
     @Override
     public void execute(ExpenseList expenseList, Ui ui, Storage storage, Categories categories)
             throws FinTrackException {
-        ui.printBorder();
-        ui.showMessage("Your current income: ");
-        ui.showMessage(Integer.toString(Savings.getIncome()));
-        int newIncome = parser.readInt("Enter your new income:");
-        if (newIncome > 0) {
-            Savings.updateIncome(newIncome);
-            storage.saveSavingsToFile(newIncome, Savings.getSavingsGoal(), Savings.getCurrentSavings());
-            ui.showMessage("Income updated to " + newIncome + ".");
-        } else {
-            ui.showMessage("Income not updated as the number you entered is invalid.");
+        try{
+            ui.printBorder();
+            ui.showMessage("Your current income: ");
+            float oldIncome = (float)(Savings.getIncome()) / 100;
+            ui.showMessage(Float.toString(oldIncome));
+            int newIncome = parser.readInt("Enter your new income in dollars " +
+                    "(your income is only input in dollars) :");
+
+            if(newIncome <= 0 ){
+                throw new FinTrackException("Please enter an amount larger than 0.");
+            }
+
+            if(newIncome > MAX_EXPENSE_AMOUNT){
+                throw new FinTrackException("Please enter an amount less than $1000000.");
+            }
+
+            Savings.updateIncome(newIncome * 100);
+            Storage.saveSavingsToFile(newIncome * 100, Savings.getSavingsGoal(), Savings.getCurrentSavings());
+            float finalIncome = (float)(newIncome * 100) / 100;
+            ui.showMessage("Your income has been update to $" + finalIncome);
+            ui.printBorder();
+        } catch (FinTrackException e) {
+            ui.showError(e.getMessage());
+            ui.printBorder();
+        } catch (Exception e) {
+            ui.showError("Please enter a valid integer input" + e.getMessage());
+            ui.printBorder();
         }
-        ui.printBorder();
     }
 }
